3736e3099563e9056c9b243f59ef3773a0d16138
saroj sundara <sarojkumarsundara518@gmail.com>
2023-01-17 02:30:54 +0530

    add top level route

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import Instrument from "./components/Instrument";
+import Quote from "./components/Quote";
+import PageNotFound from "./components/PageNotFound";
 import App from './App'
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <PageNotFound />,
+    children: [
+      {
+        path: "/",
+        element: <Instrument />,
+      },
+      {
+        path: "/quotes/:symbol",
+        element: <Quote />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={appRouter} />
+);
+
