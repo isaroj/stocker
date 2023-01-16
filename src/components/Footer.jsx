bf4cb47163859e98a378bfba6b7730389c1ee49b
saroj sundara <sarojkumarsundara518@gmail.com>
2023-01-17 02:32:20 +0530

    Create Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import { FaHeart, FaReact } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div
+      className="bg-dark text-white text-center fixed-bottom"
+      style={{
+        // position: 'fixed',
+        // bottom: 0,
+        width: "100vw",
+      }}
+    >
+      <div style={{ fontSize: "1.2rem" }} className="p-3">
+        Made with <FaHeart className="text-danger" /> in React{" "}
+        <FaReact className="text-info" />
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
+
+const footerStyle = {
+  main: {
+    position: "absolute",
+    bottom: "0",
+  },
+};
