898b8983be4a8949fee2d4a342bfa43e05bf4e98
saroj sundara <sarojkumarsundara518@gmail.com>
2023-01-17 13:17:43 +0530

    import css file for toast

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Footer from './components/Footer'
 import { Outlet } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.css";
+import "react-toastify/dist/ReactToastify.css";
 
 import './App.css'
 
