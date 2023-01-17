bca1659528f5250a7530d94d15f8735daf63afa4
saroj sundara <sarojkumarsundara518@gmail.com>
2023-01-17 13:15:29 +0530

    Fetch CSV data & parsing

--- src/components/Instrument.jsx
+++ src/components/Instrument.jsx
@@ -1,9 +1,50 @@
+import {useEffect, useState} from 'react';
+import Papa from "papaparse";
+
+import { toast, ToastContainer } from "react-toastify";
+
+
+// services
+import {instrumentService} from '../services/InstrumentsService'
+
 const Instrument = () => {
+    const [file, setFile] = useState([]);
+
+     const parseFile = (file) => {
+       Papa.parse(file, {
+         header: true,
+         complete: (data) => {
+           console.log(data);
+         },
+       });
+     };
+
+    const getAllInstruments = async () => {
+        try {
+            const {data} = await instrumentService()
+            debugger
+            parseFile(data)
+           
+        } catch (error) {
+            toast.error("Oops! something went wrong!");
+        }
+    }
+
+    useEffect(() => {
+        getAllInstruments()
+    }, []);
     return (
-        <div>
-            <h1>Instruemnt</h1>
-        </div>
-    )
+      <div>
+        <ToastContainer
+          position="top-right"
+          closeOnClick
+          draggable
+          pauseOnHover
+          theme="light"
+        />
+        <h1>Instruemnt</h1>
+      </div>
+    );
 }
 
 export default Instrument
