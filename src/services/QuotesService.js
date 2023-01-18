5b2bb74c4937de0523f677e348856d42c1186b29
saroj sundara <sarojkumarsundara518@gmail.com>
2023-01-18 12:06:35 +0530

    rename method

--- src/services/QuotesService.js
+++ src/services/QuotesService.js
@@ -2,7 +2,7 @@ import Axios from "axios";
 
 const url = "https://prototype.sbulltech.com/api/v2/quotes";
 
-export const instrumentService = (endPoint) => {
+export const quotesService = (endPoint) => {
   const fetchUrl = url+`/${endPoint}`
   return Axios.get(fetchUrl);
 };
